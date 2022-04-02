76c689f8e8a3acb9633f6d1be7abaa2984ba9801
Jatin <sjatin1308@gmail.com>
2022-04-02 12:58:51 +0530

    Sticky Sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,15 +1,18 @@
 import { Dashboard, LabelImportant, Settings, Speed, Widgets } from '@mui/icons-material'
 import React from 'react'
 import "./sidebar.css"
+import StickyBox from "react-sticky-box";
 
 export default function Sidebar() {
   return (
     <div className='sidebar'>
+      <StickyBox offsetTop={10} offsetBottom={10}>
         <div className="icon top"><Dashboard/><span>Dashboard</span></div>
         <div className='icon'><LabelImportant/><span>Transactions</span></div>
         <div className='icon'><Widgets/><span>Entites</span></div>
         <div className='icon'><Speed/><span>Health</span></div>
         <div className="icon bot"><Settings/><span>Settings</span></div>
+        </StickyBox>
     </div>
   )
 }
