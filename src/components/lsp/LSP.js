ab31f4371ad46bd940ea3962291d3c16d0a5ce46
SHEIKHMOHAMMEDUMAR <sheikhmdumar.ec19@rvce.edu.in>
2022-04-11 19:24:29 +0530

    updated

--- src/components/lsp/LSP.js
+++ src/components/lsp/LSP.js
@@ -1,18 +1,9 @@
 import "./lsp.css"
 import React from "react";
 import ContentLoader from "react-content-loader"
-import { Cancel, CheckCircle } from "@mui/icons-material";
 import { DataGrid } from "@mui/x-data-grid";
 
-export default function LSP({items, isLoading}) {
-  const logo = ({item}) => {
-    if (item === "Approved"){
-      return <CheckCircle className="Approved"/>
-    }
-    else{
-      return <Cancel className="Declined"/>
-    }
-  }  
+export default function LSP({items, isLoading}) {  
   const cols = [
   { field: "lsp", headerName: "LSP", width: 100 },
   { field: "l_type", headerName: "Loan type", width: 200 },
@@ -35,8 +26,8 @@ export default function LSP({items, isLoading}) {
       <DataGrid
         rows={rows}
         columns={cols}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
         checkboxSelection
         disableSelectionOnClick/>
     </div>
@@ -44,8 +35,8 @@ export default function LSP({items, isLoading}) {
     <div className="table">
       <DataGrid
         columns={cols}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
         checkboxSelection
         disableSelectionOnClick/>
       <span>
@@ -57,59 +48,11 @@ export default function LSP({items, isLoading}) {
     
   )
 }
-
-
-
-
-
-/*import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
-isLoading ? (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 200 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">LSP</TableCell>
-            <TableCell align="left">Loan Type</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="left">Date - Time</TableCell>
-            <TableCell align="left">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map((i) => (
-            <TableRow>
-              <TableCell component="th" scope="row">
-                {i.lsp}
-              </TableCell>
-              <TableCell align="left">{i.l_type}</TableCell>
-              <TableCell align="right">{i.Amt}</TableCell>
-              <TableCell align="left">{i.DT}</TableCell>
-              <TableCell align="left"><span className="status">{i.Status}</span>
-                                      <span className="logo">{logo({item:i.Status})}</span>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  ) : (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 200 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">LSP</TableCell>
-            <TableCell align="left">Loan Type</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="left">Date - Time</TableCell>
-            <TableCell align="left">Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        </TableBody>
-      </Table>
-      <ContentLoader  viewBox="0 0 380 700" speed={1.5}>
-            <rect x="15" y="10" rx="2" ry="2" width="360" height="500" />
-            </ContentLoader>
-    </TableContainer>
-
-  )*/
+/*const logo = ({item}) => {
+    if (item === "Approved"){
+      return <CheckCircle className="Approved"/>
+    }
+    else{
+      return <Cancel className="Declined"/>
+    }
+  }*/
